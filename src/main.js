c751e2f9b400d868254e5aebec929bc3668639d6
Nobii <steliosalvanos@gmail.com>
2025-06-20 16:37:36 +0300

    temp commit

--- src/main.js
+++ src/main.js
@@ -1,75 +1,84 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import { Pane } from "tweakpane";
+import Stats from "stats.js"
+import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree, MeshBVHHelper} from 'three-mesh-bvh';
 
 import Toolbox from "./toolbox/toolbox.js";
+import Tools from "./toolbox/toolbox.js"
+
+THREE.Mesh.prototype.raycast = acceleratedRaycast;
+THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
+THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 
 
-function main() {
-    setupScene();
-    setupEventHandlers();
-    setupUI();
+let params = {
+    visualizeBounds: false,
+    visualBoundsDepth: 10,
 };
 
-function setupScene() {
-    // Dimentions
-    const w = window.innerWidth;
-    const h = window.innerHeight;
+let stats;
+let scene, camera, renderer, controls, boundsViz;
+let mesh;
+
+
+function init() {
+    const bgColor = 0x263238 / 2;
 
     // Renderer
-    const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-    });
-    renderer.setSize(w, h);
-    document.body.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer( {antialias: true} );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor( bgColor, 1 );
+    document.body.appendChild( renderer.domElement );
 
-    // Camera
-    const fov = 75;
-    const aspect = w/h;
-    const near = 0.1;
-    const far = 10;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+    // Scene setup
+    scene = new THREE.Scene();
+    
+    const light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+    scene.add(new THREE.AmbientLight(0xffffff, 1.2));
 
-    // Scene
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xb0b0ff);
+    // Geometry setup
+    const radius = 1;
+    const tube = 0.4;
+    const tubularSegments = 400;
+    const radialSegments = 100;
 
-    // Controls
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    // Geometry
-    const geometry = new THREE.IcosahedronGeometry(1.0, 10);
+    const knotGeometry = new THREE.TorusKnotGeometry(radius, tube, tubularSegments, radialSegments);
     const material = new THREE.MeshStandardMaterial({
         color: 0x909090,
-        // wireframe: true,
-        flatShading: true,
-    });
-
-    // Light
-    const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-    scene.add( light );
-
-    // Mesh
-    const mesh = new THREE.Mesh(geometry, material);
+        // flatshading: true,
+    })
+    mesh = new THREE.Mesh(knotGeometry, material);
+    mesh.geometry.computeBoundsTree();
     scene.add(mesh);
 
-    // Toolbox
-    Toolbox.prepareforSelection(mesh, renderer, camera);
+    // Camera setup
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 50);
+    camera.position.set(3, 3, 3);
+    camera.far = 100;
+    camera.updateProjectionMatrix();
 
-    // Render Scene
-    function animate(t=0) {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-        controls.update();
-    };
-    animate();
+    // Controls
+    controls = new OrbitControls(camera, renderer.domElement);
+    
+    // Stats
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    const tools = new Tools(scene, camera, renderer, mesh);
 };
 
 function setupEventHandlers() {
-    let el = document;
+    // On resize
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
 
-    // Setup event hanlders
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }, false );
 };
 
 function setupUI() {
@@ -114,4 +123,40 @@ function setupUI() {
     });
 };
 
-main();
+function updateFromOptions() {
+    if (boundsViz && !params.visualizeBounds) {
+        scene.remove(boundsViz);
+        boundsViz = null;
+    }
+
+    if (!boundsViz && params.visualizeBounds) {
+        boundsViz = new MeshBVHHelper(mesh);
+        scene.add(boundsViz);
+    }
+
+    if (boundsViz) {
+        boundsViz.depth = params.visualBoundsDepth;
+    }
+}
+
+let lastTime = window.performance.now();
+function render () {
+    const delta = window.performance.now() - lastTime;
+    lastTime = window.performance.now();
+    
+    stats.begin();
+
+    if (boundsViz) boundsViz.update();
+
+    renderer.render(scene, camera);
+    stats.end();
+
+    requestAnimationFrame(render);
+};
+
+
+init();
+setupUI();
+setupEventHandlers();
+updateFromOptions();
+render();
--- src/toolbox/toolbox.js
+++ src/toolbox/toolbox.js
@@ -11,18 +11,96 @@ THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
 
 // Import and re-export Scene
-import Scene from "./scene.js";
 import Utils from "./utils.js";
 import GeometryHelpers from "./geomhelpers.js";
 
 const Toolbox = {
-    Scene,
     Utils,
     GeometryHelpers
 };
 
-Toolbox.prepareforSelection = (mesh, renderer, camera) => {
-    Scene.init(mesh, renderer, camera);
+class Tools {
+    constructor(scene, camera, renderer, mesh, optParams) {
+        this.scene    = scene;
+        this.camera   = camera;
+        this.renderer = renderer;
+        this.mesh     = mesh;
+
+        this._screenPointerCoords = new THREE.Vector2(0.0, 0.0);
+
+        this._bPauseQuery  = false;
+        this._bInitialized = false;
+
+        this.init();
+    }
+    
+    init() {
+        this.raycaster = new THREE.Raycaster();
+        this.raycaster.firstHitOnly = true;
+        
+        if (!this.mesh.geometry.boundsTree) {
+            console.log("No bounds tree, computing bounds tree");
+            this.mesh.geometry.computeBoundsTree();
+        }
+        this.initMeshMat();
+        this.initEventListeners();
+    }
+
+    initEventListeners() {
+        let el = this.renderer.domElement;
+
+        el.addEventListener('mousemove', this._updateScreenMove, false);
+        el.addEventListener('mousemove', this._query, false);
+    }
+
+    initMeshMat() {
+        this.mesh.material.vertexColors = true;
+        this.mesh.material.needsUpdate  = true;
+
+        if (!this.mesh.geometry.attributes.color) {
+            console.log("Initializing color");
+            let colorArray, colorAttr;
+            colorArray = new Float32Array(this.mesh.geometry.attributes.position.count * 3);
+            colorArray.fill(new THREE.Color(0xffffff)); // Default to white
+            colorAttr = new THREE.BufferAttribute(colorArray, 3);
+            this.mesh.geometry.setAttribute('color', colorAttr);
+        }
+    }
+
+    _query() {
+        if (!this._bPauseQuery) return;
+
+        this._hits = [];
+
+        this.raycaster.setFromCamera(this._screenPointerCoords, this.camera);
+        this.raycaster.intersectObject(this.scene.children, true, this._hits);
+
+        let hitsnum = this._hits.length;
+        if (hitsnum <= 0) {
+            this._queryData = undefined;
+            return;
+        }
+
+        const h = this._hits[0];
+        this._queryData = {};
+        this._queryData.p = h.point;
+        this._queryData.d = h.distance;
+        this._queryData.o = h.object;
+
+        // Compute normals
+        console.log(this._queryData)
+    }
+
+    _updateScreenMove(e) {
+        if (e.preventDefault) e.preventDefault();
+
+        this._screenPointerCoords.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+        this._screenPointerCoords.y = ( e.clientY / window.innerHeight ) * 2 - 1;
+    }
+
+    brush() {
+
+    }
 };
 
-export default Toolbox;
+export default Tools;
